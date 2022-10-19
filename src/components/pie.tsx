c09d477cf714bb425e04c52f17d952a40f70e3a3
Tarsisius Hayo <trsharyo@gmail.com>
2022-10-19 10:52:01 +0700

    update pie style

--- src/components/pie.tsx
+++ src/components/pie.tsx
@@ -6,13 +6,34 @@ type Props = {
 }
 
 const Pie: FC<Props> = (props) => {
+
+  const lineWidth = 60
   return (
     <div className='pie'>
       <PieChart
         data={[
-          { title: 'Sad', value: props.data.sad, color: 'rgb(239 68 68 / 1)' },
-          { title: 'Happy', value: props.data.happy, color: 'rgb(34 197 94 / 1)' },
+          {
+            title: 'Sad',
+            value: props.data.sad,
+            color: 'rgb(239 68 68 / 1)',
+          },
+          {
+            title: 'Happy',
+            value: props.data.happy,
+            color: 'rgb(34 197 94 / 1)',
+          },
         ]}
+        radius={44}
+        segmentsStyle={{ transition: 'stroke .3s', cursor: 'pointer' }}
+        lineWidth={lineWidth}
+        label={({ dataEntry }) => Math.round(dataEntry.percentage) + '%'}
+        labelPosition={100 - lineWidth / 2}
+        labelStyle={{
+          fill: '#fff',
+          opacity: 0.75,
+          pointerEvents: 'none',
+          fontSize: '12px',
+        }}
       />
     </div>
   )
